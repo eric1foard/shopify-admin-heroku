771cf957033fc6f355244a26713094deaf728538
Eric Foard <foard265@gmail.com>
2018-07-25 12:31:29 -0700

    string

--- client/reducers/reducer_products.js
+++ client/reducers/reducer_products.js
@@ -1,9 +1,9 @@
 const initState = [];
 
 export default function reducer(state = initState, action) {
-    switch (action.type) {
-      case ADD_SELECTED_PRODUCTS:
-        return state.concat(action.payload);
-    }
-    return state;
+  switch (action.type) {
+    case 'ADD_SELECTED_PRODUCTS':
+      return state.concat(action.payload);
   }
+  return state;
+}
