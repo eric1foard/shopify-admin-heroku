71013ed46619588b704586357e66e7239b1c0108
Eric Foard <foard265@gmail.com>
2018-08-01 13:53:30 -0700

    remove src

--- client/components/EditModal.js
+++ client/components/EditModal.js
@@ -5,7 +5,7 @@ import ProductEditForm from './ProductEditForm';
 class EditModal extends Component {
     render() {
         return <Modal
-            src="https://my-app.com/upgrade-to-retail-package"
+            // src="https://my-app.com/upgrade-to-retail-package"
             open={this.props.isEditModalOpen}
             title="Upgrade your Shopify POS with the Retail Package"
             primaryAction={{
