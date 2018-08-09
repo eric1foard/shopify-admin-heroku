94706cb229036cbf110ae0aaeccce58fd330d9af
Eric Foard <foard265@gmail.com>
2018-08-09 11:45:47 -0700

    photo in product list avatar

--- utils/image.js
+++ utils/image.js
@@ -1,7 +1,3 @@
 export function hasValidDimensions(height, width) {
     return Boolean(height && width);
 }
-
-export function isLowResolution(image) {
-    return image && image.resolution_dpi < 300;
-}
