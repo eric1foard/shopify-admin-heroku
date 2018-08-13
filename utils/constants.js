239d016c570b14bbe73971f1a8dd32ef27abd755
Eric Foard <foard265@gmail.com>
2018-08-13 11:10:19 -0700

    add image and dimensions filters

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,11 @@
 module.exports = {
     AR_PRODUCT_TAG: 'AR_CLIENT_ENABLED',
     ACCEPTED_FILETYPES: ['.gif', '.jpg', '.png', '.svg'],
-    EMPTY_IMAGE: "https://d1yt4mgfxmh02g.cloudfront.net/empty-product-image.png"
+    EMPTY_IMAGE: 'https://d1yt4mgfxmh02g.cloudfront.net/empty-product-image.png',
+    IMAGE_NEEDED_FILTER: 'imageNeededFilter',
+    DIMENSIONS_NEEDED_FILTER: 'dimensionsNeededFilter',
+    FILTER_OPTS: {
+        NEEDED: 'needed',
+        PRESENT: 'present'
+    }
 }
