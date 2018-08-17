cf5c55e984797aa1a1ea81517d91a4c2871929b8
Eric Foard <foard265@gmail.com>
2018-08-17 11:55:16 -0700

    fix breadcrumb url

--- client/reducers/reducer_page.js
+++ client/reducers/reducer_page.js
@@ -1,4 +1,6 @@
 import { MAIN_PAGE_NAME } from '../../utils/constants';
+import config from 'config';
+const url = config.get('breadcrumbURL');
 
 const initState = {
   title: MAIN_PAGE_NAME,
@@ -12,7 +14,7 @@ export default function reducer(state = initState, action) {
           return {
             title,
             breadcrumbs: title === MAIN_PAGE_NAME ? [] :
-            [{content: MAIN_PAGE_NAME, url: '/', accessibilityLabel: `Back to ${MAIN_PAGE_NAME}`}]
+            [{content: MAIN_PAGE_NAME, url, accessibilityLabel: `Back to ${MAIN_PAGE_NAME}`}]
           };
   }
   return state;
